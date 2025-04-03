64d73e99eb2ac46487d095db364fe68fdab1a467
TanHuy1008 <huylaqn1008@gmail.com>
2025-04-03 16:33:38 +0700

    Add animation slider

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,293 @@
-import React from 'react'
+import React from "react";
+import CircleSlider from "./slider";
 
 const page = () => {
   return (
-    <h1 className='font-pregular'>Hello World</h1>
-  )
-}
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Before they sold out
+              <br className="hidden lg:inline-block" />
+              readymade gluten
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+              park mlkshk tote bag selvage hot chicken authentic tumeric
+              truffaut hexagon try-hard chambray.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Button
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://scontent.fsgn2-8.fna.fbcdn.net/v/t39.30808-6/460977765_1151625209913368_6942620315579103678_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeF4-V3A59THSKSmPnqBQefW26b5uOYhZSLbpvm45iFlIt0C7kt2Y5sjXsoskRSxEesZNq4_42NeE-WOzzmL-EqT&_nc_ohc=bOSG6Xo-JfsQ7kNvgEh72LL&_nc_oc=AdnQ_iFFR-g8DXI93GwrdpnuE7p9o9I_5cnVCbaQ5DUJDwqlaCe1uY_pmowr7BtnqpyGA2BPeCk-kCDrSYdON0SE&_nc_zt=23&_nc_ht=scontent.fsgn2-8.fna&_nc_gid=aAuJW300KekG7rXEpnAaeA&oh=00_AYFde98bN_VHo7nBa51LqqDDhSJOVEzUElz-0rOhDoqdKw&oe=67E865A7"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full mb-20">
+            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                Pitchfork Kickstarter Taxidermy
+              </h1>
+              <div className="h-1 w-20 bg-indigo-500 rounded" />
+            </div>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Chichen Itza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/721x401"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/722x402"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/723x403"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  San Francisco
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              Our Team
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-2">
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/80x80"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Holden Caulfield
+                  </h2>
+                  <p className="text-gray-500">UI Designer</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/84x84"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Henry Letham
+                  </h2>
+                  <p className="text-gray-500">CTO</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/88x88"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Oskar Blinde
+                  </h2>
+                  <p className="text-gray-500">Founder</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/90x90"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    John Doe
+                  </h2>
+                  <p className="text-gray-500">DevOps</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/94x94"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Martin Eden
+                  </h2>
+                  <p className="text-gray-500">Software Engineer</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/98x98"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Boris Kitua
+                  </h2>
+                  <p className="text-gray-500">UX Researcher</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/100x90"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Atticus Finch
+                  </h2>
+                  <p className="text-gray-500">QA Engineer</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/104x94"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Alper Kamu
+                  </h2>
+                  <p className="text-gray-500">System</p>
+                </div>
+              </div>
+            </div>
+            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                <img
+                  alt="team"
+                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                  src="https://dummyimage.com/108x98"
+                />
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 title-font font-medium">
+                    Rodrigo Monchi
+                  </h2>
+                  <p className="text-gray-500">Product Manager</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font">
+        <CircleSlider/>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default page;
